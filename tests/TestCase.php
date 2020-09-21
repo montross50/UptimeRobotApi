dd373ffa2b053cc5ebcc29d6cfdec544356c7623
trent.schmidt <trent.schmidt@egov.com>
2020-09-21 00:13:05 -0500

    Style Fixes

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,16 +6,13 @@ use Http\Discovery\Strategy\MockClientStrategy;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
-    public function setUp(): void 
+    public function setUp(): void
     {
         parent::setUp();
-        
     }
     
     protected function getPackageProviders($app)
     {
         return [\Montross50\UptimeRobotApi\UptimeRobotApiServiceProvider::class];
     }
-
-    
 }
